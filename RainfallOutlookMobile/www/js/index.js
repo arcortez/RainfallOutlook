03445d43b2117b6d50095549991a07a136943bfd
arcortez <arcortez@up.edu.ph>
2018-11-14 00:16:47 +0800

    fix feel

--- RainfallOutlookMobile/www/js/index.js
+++ RainfallOutlookMobile/www/js/index.js
@@ -1,50 +1,121 @@
+var currentPage = 'home';
+
 function openNav() {
-    document.getElementById("mySidenav").style.left = "0";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("mySidenav").style.display = "block";
+    document.getElementById("menuOpen").style.display = "none";
+    document.getElementById("menuClose").style.display = "block";
+    document.getElementById("headlogo").style.visibility = "hidden";
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.left = "-1000";
-    document.body.style.backgroundColor = "white";
+    document.getElementById("mySidenav").style.display = "none";
+    document.getElementById("menuOpen").style.display = "block";
+    document.getElementById("menuClose").style.display = "none";
+    document.getElementById("headlogo").style.visibility = "visible";
+    closeTip();
+}
+
+function openTip(){
+    document.getElementById("tiptag").style.display = "none";
+    document.getElementById("agritips").style.display = "block";
+}
+
+function closeTip(){
+    document.getElementById("tiptag").style.display = "block";
+    document.getElementById("agritips").style.display = "none";   
 }
 
 function openPage(string){
-    document.getElementById("maps").style.display = "none";
+    document.getElementById("scheme").style.display = "none";
+    document.getElementById("home").style.display = "none";
     document.getElementById("data").style.display = "none";
     document.getElementById("downloads").style.display = "none";
     document.getElementById("settings").style.display = "none";
-    document.getElementById("arrowright").style.display = "none";
-    document.getElementById("arrowleft").style.display = "none";
-    document.getElementById("legend").style.display = "none";
-    document.getElementById("floatingbtn").style.display = "none"
-    document.getElementById("refresh").style.display = "none";
-    document.getElementById("backtomaps").style.display = "none";
 
-    if(string =='maps'){
+    if(string =='home'){
+        document.getElementById("home").style.display = "block"; 
+        currentPage = 'home';
+    }else if(string =='maps'){
         document.getElementById("maps").style.display = "block"; 
-        document.getElementById("arrowright").style.display = "block";
-        document.getElementById("arrowleft").style.display = "block";
-        document.getElementById("floatingbtn").style.display = "block";
+        currentPage = 'maps';
     }else if(string =='data'){
         document.getElementById("data").style.display = "block";
-        document.getElementById("floatingbtn").style.display = "block";
-        document.getElementById("refresh").style.display = "block";    
-        document.getElementById("backtomaps").style.display = "block";    
+        currentPage = 'data';
     }else if(string =='downloads'){
         document.getElementById("downloads").style.display = "block";
+        currentPage = 'downloads';
     }else if(string =='settings'){
         document.getElementById("settings").style.display = "block";
+        currentPage = 'settings';
     }
     closeNav();
 }
 
-function showLegend(bool){
+document.getElementById('displays').provinceSelect.onchange = changeP;
+
+function changeP(){
+    var changeID = document.getElementById('displays').provinceSelect.value;
+    var loaded = angular.element(document.getElementById("fullscale")).scope();
+    //trigger angular function in controller.js
+    loaded.$apply(function(){
+        loaded.changeCurrentProvince(changeID);
+    })
+}
+
+
+document.getElementById('displays').muniSelect.onchange = changeM;
+
+function changeM(){
+    var changeID = document.getElementById('displays').muniSelect.value;
+    var loaded = angular.element(document.getElementById("fullscale")).scope();
+
+    //trigger angular function in controller.js
+    loaded.$apply(function(){
+        loaded.changeDisplayArr(changeID);
+    })
+}
+
+
+function selectScheme(){
+    document.getElementById('notices').innerHTML = "Loading... Please Wait. This might take a bit."
+    d3.csv("data/Forecast.csv", parseData);
+    d3.json("data/Provinces.json", drawMaps);
+    document.getElementById('menuOpen').style.display = 'block'; 
+    document.getElementById('main').style.display = 'block';
+    document.getElementById('page-header').style.display = 'block';
+    document.getElementById('footer').style.display = 'block';
+    openPage('home');
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
+
+function onDeviceReady() {
     
-    if(bool == 'true'){
-        document.getElementById("legend").style.display = "block";
-        console.log("show legend");
+}
+
+function onBackKeyDown(e) {
+    e.preventDefault();
+    closeNav();
+    if(currentPage != 'home'){
+        openPage('home')
     }else{
-        document.getElementById("legend").style.display = "none";
-        console.log("hide legend");
+        var conf = confirm("Would you like to exit the app?");
+        if(conf == true){
+            App.exitApp();
+        }else{
+            //do nothing
+        }
     }
+}
+
+
+
+function setM(val){
+    document.getElementById('displays').muniSelect.value = val.id;
+    changeM();
+}
+
+function setP(){
+    document.getElementById('displays').pkey.value
+    document.getElementById('displays').provinceSelect.value = val.id;
+    changeP();
 }
--- RainfallOutlookMobile/www/js/mapper.js
+++ RainfallOutlookMobile/www/js/mapper.js
@@ -1,3 +1,4 @@
+  
 $(window).resize(function() {
  
   svg
@@ -5,18 +6,37 @@ $(window).resize(function() {
     .attr("height", $(".map-holder").height())
   ;
   
-});
+}); //for debugging/testing
 
-function getColorVal(num){
-	if(num < 40) return "rgb(255,0,0)";
-	else if(num >= 40 && num <80) return "rgb(255,255,0)";
-	else if(num >=80 && num <=120) return "rgb(0,176,40)";
-	else if(num > 120) return "rgb(68,114,198)";
-	else console.log('invalid input >> '+num);
+function getColorVal(num, color_schema){
+	if(color_schema == "FOUR"){
+		if(num < 100) return "rgb(255,0,0)";
+		else if(num >= 100 && num <250) return "rgb(255,255,0)";
+		else if(num >=250 && num <=500) return "rgb(0,176,40)";
+		else if(num > 500) return "rgb(68,114,198)";
+		else console.log('invalid input >> '+num);
+	}else if(color_schema == "BLUE"){
+
+		if(num < 100) return "rgb(225,225,225)";
+		else if(num >= 100 && num <250) return "rgb(116,220,253)";
+		else if(num >=250 && num <=500) return "rgb(9,87,235)";
+		else if(num > 500) return "rgb(30,59,180)";
+		else console.log('invalid input >> '+num);
+
+		/* //6 shades of blue 
+		if(num < 50) return "rgb(225,225,225)";
+		else if(num >= 50 && num < 100) return "rgb(185,231,253)";
+		else if(num >= 100 && num < 200) return "rgb(16,183, 252)";
+		else if(num >= 200 && num < 300) return "rgb(10,109,254)";
+		else if(num >= 300 && num < 400) return "rgb(5,76,156)";
+		else if(num >= 400 && num <= 500) return "rgb(0,38,118)";
+		else return "rgb(0,10,10)";
+		*/ 
+	}
 }
 
-function getTextColor(num){
-	if(num >= 40 && num <80) return "black";
+function getTextColor(num, color_schema){
+	if(num < 250) return "black";
 	else return "white"
 }
 
@@ -27,11 +47,11 @@ var svg = d3.selectAll(".map-holder")
   
 var width = $(".map-holder").width();
 var height = $(".map-holder").height();
-var scale_value = 1700;
+var scale_value = 500;
 
 var projection = d3
 	.geoEquirectangular()
-	.center([132, 13]) //the lat-long degrees of the philippines
+	.center([170, -4]) //the lat-long degrees of the philippines
 	.scale(scale_value);
 
 var path = d3
@@ -45,41 +65,125 @@ function parseProv(data){
 	for(let each of data){
 		provlist.push(each.Provinces);
 	}
+
 }
+
+var colorschemes = [
+	{
+		id: 0,
+		schemename: 'BLUE', 
+		schemedesc: "Shades of Blue",
+		array:[
+			{
+				color: "rgb(225,225,225)",
+				desc: "< 100mm",
+				// lower_range: 0,
+				// upper_range: 100
+			},
+			{
+				color: "rgb(116,220,253)",
+				desc: "100-250mm"
+			},
+			{
+				color: "rgb(9,87,235)",
+				desc: "250-500mm"
+			},
+			{
+				color: "rgb(30,59,180)",
+				desc: "> 500mm"
+			}
+		] 
+	},
+	{
+		id: 1,
+		schemename: 'FOUR', 
+		schemedesc: "Four Colors (Red, Yellow, Green, Blue)",
+		array:[
+			{
+				color: "rgb(255,0,0)",
+				desc: "< 100mm"
+			},
+			{
+				color: "rgb(255,255,0)",
+				desc: "100-250mm"
+			},
+			{
+				color: "rgb(0,176,40)",
+				desc: "250-500mm"
+			},
+			{
+				color: "rgb(68,114,198)",
+				desc: "> 500mm"
+			}
+		] 
+	}
+];
 
 var allMunicipalities = [];
 var monthspan = [];
-d3.csv("data/Forecast.csv", parseData)
-function parseData(data){
+var colorarr;
 
-	monthspan = data.columns;
-	monthspan.shift();
+
+function parseData(data){
+	// console.log(data)
+
+    var scheme = document.getElementById('cschema').value; // <input> in div#schema
+    // console.log(scheme);
+
+    if(scheme == 'FOUR'){ //taking from predefined colorarr
+    	colorarr = colorschemes[1];
+    }else if(scheme == 'BLUE'){
+    	colorarr = colorschemes[0];
+    }
+
+	monthspan = data.columns; // will be used late in controller.js
+	monthspan.shift(); // removes first column header of csv file, uneeded
 	console.log(monthspan);
 
-	data.push({Municipality:"end"});
+	data.push({Municipality:"end"}); // to mark end of iteration in parsing
 
-	var currentProvince = provlist.shift();
+	var currentProvince = provlist.shift(); //dequeue
 	var nextProvince = provlist.shift();
 	
-	var provinceCount = 0;
+	var provinceCount = 0; // for IDs
+	var municipalityCount = 0;
 
 	var pMuni = [];
 	for(let each of data){
 		var currProvAsObject;
 		var arrayofvalues = [];
-		if(each.Municipality == "Abra"){
+
+		var keylist = Object.keys(each);
+		var entryString = each[keylist[0]]; // array of objects do not have index, a workaround
+
+		// console.log(each); //checking by sight
+
+		if(entryString == "Abra"){ 
 			currProvAsObject = each;
 			continue;
 		}
 
-		if(each.Municipality == nextProvince || each.Municipality == "end"){
+		/*
+		CSV File Format:
+		
+		Prov/Muni   Month1 Month2 ...  Month6
+		Prov
+		Muni
+		Muni
+		...
+		Prov
+	
+		*/
+
+		if(entryString == nextProvince || entryString == "end"){ // if province is encountered.
 			var keys = Object.keys(currProvAsObject);
 			for(let i=1;i<keys.length;i++){
 				var val = currProvAsObject[keys[i]];
 				arrayofvalues.push({
+					id: i-1,
 					value: val,
-					color: getColorVal(val),
-					textcolor: getTextColor(val)
+					color: getColorVal(val, scheme),
+					textcolor: getTextColor(val, scheme)
 				});
 			}
 
@@ -89,13 +193,15 @@ function parseData(data){
 				array: arrayofvalues,
 				municipalities: pMuni
 			});
+
 			provinceCount++;
+			municipalityCount = 0;
 			pMuni = [];
 			currentProvince = nextProvince;
 			nextProvince = provlist.shift();
 			currProvAsObject = each;
 		}else{
-			var name = each.Municipality
+			var name = entryString
 			var arr = [];
 			var carr = [];
 
@@ -104,27 +210,35 @@ function parseData(data){
 			for(let i=1;i<len;i++){
 				var temp = each[keys[i]]
 				arr.push({
+					id: i-1,
 					value: temp,
-					color: getColorVal(temp),
-					textcolor: getTextColor(temp)					
+					color: getColorVal(temp, scheme),
+					textcolor: getTextColor(temp, scheme)					
 				});
 			}
-
 			var obj = {
+				id: municipalityCount,
 				Municipality: name,
 				array: arr
 			}
+			municipalityCount += 1;
 			// console.log(each);
 			// console.log(arr);
 			pMuni.push(obj);
 		}
 	}
-	console.log(allMunicipalities)
+	// console.log(allMunicipalities)
+
 }
 
-// d3.json("data/Provinces.json", drawMaps);
-function drawMaps (geojson) {
+
+
+//d3js
+function drawMaps (geojson){
+	console.log('mapping')
 	var t = 0;
+	var m = 0;
+	var p = 0;
 	var provinces = svg.append("g").attr("class", "geo");
 	
 	provinces
@@ -135,6 +249,19 @@ function drawMaps (geojson) {
 		.attr("height", height)
 		.attr("fill", "white");
 
+	provinces
+		.append("text")
+		.attr("x", 25)
+		.attr("y", 25)
+		.attr("fill", "#d0d0d0")
+		.attr("style", "opacity: .8;")
+		.attr("font-size", "14px")
+		.text(function(d){
+			var string = monthspan[p].toUpperCase();
+			p++;
+			console.log(string);
+			return string;
+		});
 
 	var prov = provinces
 		.selectAll("path")
@@ -143,94 +270,27 @@ function drawMaps (geojson) {
 		.append("path")
 		.attr("d", path)
 		.attr("fill", function(d){
-			var val = Math.floor((Math.random() * 7) + 1);
-			var string = "";
-
-			if(val == 1){
-				string = "rgb(255,0,0)";
-			}else if(val == 2){
-				string = "rgb(255,255,0)";
-			}else if(val == 3){
-				string = "rgb(0,176,80)";
-			}else{
-				string = "rgb(68,114,196)";
+			var provnumberID = parseInt(d.properties.ID_1) - 1;
+			var colorstring = allMunicipalities[provnumberID].array[m].color	
+			if(d.properties.ID_1 == 82){
+				m++;
 			}
-			return string;
+			return colorstring;
 		})
 		.attr("id", function(d){
 			if(d.properties.ID_1 == 82){
 				t++;
 			}
+			// console.log("PH"+t+"-"+d.properties.ID_1);
 			return "PH"+t+"-"+d.properties.ID_1			
 		})
 		.on("click", function(d){
 			console.log(d.properties.ID_1+" "+d.properties.NAME_1)
 
-		})
-		.on("mouseover", function(d){
-			d3.select("#PH"+t+"-"+d.properties.ID_1).classed("selected", true);
-			d3.select("#PL"+t+"-"+d.properties.ID_1).style("display", "block");
 		});
-
-	var provincenames = svg.append("g").attr("class", "label");
-	var h = 0;
-
-	var provnames = provincenames
-		.selectAll("path")
-		.data(geojson.features)
-		.enter()
-		.append("g")
-		.attr("class", "provincenames")
-		.attr("style", "display: none;")
-      	.attr("transform", function(d) {
-        	return (
-	          	"translate(" + path.centroid(d)[0] + "," + path.centroid(d)[1] + ")"
-        	);
-    	});
-
-	provnames
-		.attr("id", function(d){
-			if(d.properties.ID_1 == 82){
-				h++;	
-			}
-			return "PL"+h+"-"+d.properties.ID_1;
-		})
-		.append("text")
-	    .attr("class", "provlab")
-	    .style("text-anchor", "middle")
-	    .attr("dx", 0)
-	    .attr("dy", 0)
-	    .text(function(d) {
-	    	// console.log(d)
-	    	return d.properties.NAME_1;
-	    });
-	// initiateZoom();
-
+	
+	var loaded = angular.element(document.getElementById("fullscale")).scope();
+	loaded.$apply(function(){
+		loaded.loadProvinceData();
+	})
 }
-
-
-// var zoom = d3
-//   .zoom()
-//   .on("zoom", zoomed)
-// ;
-// function zoomed() {
-//   t = d3
-//     .event
-//     .transform
-//   ;
-//   prov
-//     .attr("transform","translate(" + [t.x, t.y] + ")scale(" + t.k + ")")
-//   ;
-// }
-
-// function initiateZoom() {
-//   minZoom = Math.max($("#map-holder").width() / 133, $("#map-holder").height() / 31);
-//   maxZoom = 20 * minZoom;
-//   zoom
-//     .scaleExtent([minZoom, maxZoom])
-//     .translateExtent([[0, 0], [w, h]])
-//   ;
-//   midX = ($("#map-holder").width() - minZoom * w) / 2;
-//   midY = ($("#map-holder").height() - minZoom * h) / 2;
-//   svg.call(zoom.transform, d3.zoomIdentity.translate(midX, midY).scale(minZoom));
-// }
